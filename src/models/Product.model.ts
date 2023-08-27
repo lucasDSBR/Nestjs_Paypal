35e29e4657cf672b05b09597b3fb4da098db47c5
Lucas Silva <76401911+lucasDSBR@users.noreply.github.com>
2023-08-27 16:11:06 +0000

    create model product and serve

---
+++ src/models/Product.model.ts
@@ -0,0 +1,9 @@
+
+export class Product {
+  name: string;
+  type: string;
+  description: string;
+  category: string;
+  image_url: string;
+  home_url: string;
+}
--- src/servers/products/product.serve.ts
+++ src/servers/products/product.serve.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
-
+import { Product } from '../../models/Product.model'
 @Injectable()
 export class ProductService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createProduct(product: Product): any {
+    return "teste";
   }
 }
